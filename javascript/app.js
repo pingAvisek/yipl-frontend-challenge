c42c2f17b1f508ecc85e30e66bfdf9146fb6a0ef
Abhishek Ghimire <aghimire074@gmail.com>
2023-02-28 19:29:19 +0545

    some pressurized work on responsive

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 // Selecting all img elements and setting the false attribute to the value of the dragable attribute.
-var images = document.querySelectorAll("img");
-for (var i = 0; i < images.length; i++) {
+let images = document.querySelectorAll("img");
+for (let i = 0; i < images.length; i++) {
   images[i].setAttribute("draggable", false);
 }
